24db2de6f959a566fe8d7c69dec0bc8c552d474e
Taiju Aoki <taiju.dev@gmail.com>
2017-09-30 18:38:25 +0900

    Remove unused feature

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,20 +1,6 @@
-const fs = require('fs')
 const moment = require('moment-timezone')
 
 module.exports = {
-  readFileSync(filePath) {
-    console.log('readFileSync')
-    console.log('filePath: ' + filePath)
-    const data = fs.readFileSync(filePath, 'utf-8')
-    console.log('data: ' + data)
-    return data
-  },
-  writeFileSync(filePath, data) {
-    console.log('writeFileSync')
-    console.log('filePath: ' + filePath)
-    console.log('data: ' + data)
-    fs.writeFileSync(filePath, data)
-  },
   timeToString(time, format) {
     // timeはDate型でもunixタイムスタンプでも可
     moment.tz.setDefault('Asia/Tokyo')
